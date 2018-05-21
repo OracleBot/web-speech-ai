48288e18ce2736a53968c7db384a71e0a99c28bb
Diprish <diprish@Aradhanas-MacBook-Pro.local>
2018-05-20 21:01:02 -0500

    fixed port

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var request = {
   },
 };
 
-const server = app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT || 8080, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
